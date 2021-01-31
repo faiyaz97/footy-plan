cb266168ee50c93707b1e647f9e115085de0ee36
ffaiy001 <ffaiy001@gold.ac.uk>
2021-01-31 19:51:04 +0000

    created a file for theme

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import themeFile from './utility/theme'
 // components test
 import NavBar from './components/Navbar';
 
@@ -11,51 +12,7 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  forms: {
-   form: {
-     textAlign: "center"
-   },
-   image: {
-     margin: "10px auto 10px auto"
-   },
-   pageTitle: {
-     margin: "10px auto 10px auto"
-   },
-   textField: {
-     margin: "10px auto 10px auto"
-   },
-   button: {
-     marginTop: 20,
-     position: "relative"
-   },
-   customError: {
-     color: "red",
-     fontSize: "0.8rem",
-     marginTop: 5
-   },
-   progress: {
-     position: "absolute"
-   }
- }
-});
+const theme = createMuiTheme(themeFile);
 
 function App() {
   return (
