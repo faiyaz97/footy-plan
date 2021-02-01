386a64cbbead2f8455c2c54838b652d76e95b1d2
ffaiy001 <ffaiy001@gold.ac.uk>
2021-02-01 20:14:26 +0000

    Redux - setting up and login state

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,50 +1,62 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
-import themeFile from './utility/theme';
-import jwtDecode from 'jwt-decode';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import themeFile from "./utility/theme";
+import jwtDecode from "jwt-decode";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // components test
-import NavBar from './components/Navbar';
-import AuthRoute from './utility/AuthRoute';
+import NavBar from "./components/Navbar";
+import AuthRoute from "./utility/AuthRoute";
 
 // Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 const theme = createMuiTheme(themeFile);
 
 let authenticated;
 const token = localStorage.FBIdToken;
-if(token){
+if (token) {
   const decodedToken = jwtDecode(token);
-  if(decodedToken.exp * 1000 < Date.now()){
-    window.location.href= '/login'
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
     authenticated = false;
   } else {
     authenticated = true;
   }
-
 }
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
+      <Provider store={store}>
         <Router>
-          <NavBar/>
+          <NavBar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={home}/>
-              <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
-              <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
-
+              <Route exact path="/" component={home} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact 
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
-      </div>
+      </Provider>
     </ThemeProvider>
   );
 }
--- client-side/src/pages/login.js
+++ client-side/src/pages/login.js
@@ -12,6 +12,10 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+// Redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
+
 const styles = (theme) => ({
     ...theme.forms
   });
@@ -22,44 +26,30 @@ export class login extends Component {
         this.state = {
           email: '',
           password: '',
-          loading: false,
           errors: {}
         };
       }
+      componentWillReceiveProps(nextProps){
+          if(nextProps.UI.errors){
+            this.setState({ errors: nextProps.UI.errors });
+          }
+      }
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({
-            loading: true
-        });
         const userData = {
             email: this.state.email,
             password: this.state.password
-        }
-        axios.post('/login', userData)
-            .then(res => {
-                console.log("TEST ", res.data);
-                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                });
-            })
-
-    }
+        };
+        this.props.loginUser(userData, this.props.history);
+    };
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         });
     }
     render() {
-        const { classes} = this.props;
-        const { errors, loading } = this.state;
+        const { classes, UI: { loading }} = this.props;
+        const { errors } = this.state;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm/>
@@ -93,7 +83,19 @@ export class login extends Component {
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    loginUser: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+});
+
+const mapActionToProps = {
+    loginUser
 }
 
-export default withStyles(styles)(login);
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(login));
