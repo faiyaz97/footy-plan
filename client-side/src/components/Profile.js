0c42f01a2db201967780ff93ed320fac41847a67
ffaiy001 <ffaiy001@gold.ac.uk>
2021-03-16 13:25:02 +0000

    logout button

--- client-side/src/components/Profile.js
+++ client-side/src/components/Profile.js
@@ -15,6 +15,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 // icons
 import CalendarToday from '@material-ui/icons/CalendarToday'
 import EditIcon from '@material-ui/icons/Edit'
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn'
 
 
 // redux stuff
@@ -82,6 +83,10 @@ export class Profile extends Component {
         const fileInput = document.getElementById('imageInput');
         fileInput.click();
       };
+    handleLogout = () => {
+        this.props.logoutUser();
+    }
+
     render() {
         const { 
             classes, 
@@ -114,6 +119,11 @@ export class Profile extends Component {
                         <CalendarToday color="primary" />{' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+                    <Tooltip title="Logout" placement="top">
+                        <IconButton onClick={this.handleLogout}>
+                            <KeyboardReturn color="primary"/>
+                        </IconButton>
+                    </Tooltip>
                 </div>
             </Paper>
         ) : (
